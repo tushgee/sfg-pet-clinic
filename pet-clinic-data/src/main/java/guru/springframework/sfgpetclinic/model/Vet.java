a3b016058ccd466f5d6499ded2664aa62fce5b41
tushgee <tushgee@gmail.com>
2019-12-11 10:53:24 -0600

    added Specialty, updated Vet with specialities relationship. closes #29

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,4 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+    public Set<Specialty> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Specialty> specialities) {
+        this.specialities = specialities;
+    }
+
+    private Set<Specialty> specialities;
 }
