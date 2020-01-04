33fe8516acdc52feeae5a1e537f82360bd192427
tushgee <tushgee@gmail.com>
2020-01-03 23:50:21 -0800

    implemented repos. closes #41

---
+++ pet-clinic-data/src/main/java/repository/OwnerRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+}
---
+++ pet-clinic-data/src/main/java/repository/PetTypeRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetTypeRepository extends CrudRepository<PetType, Long> {
+}
