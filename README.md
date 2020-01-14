1d63d37d22ec1c89217539d3de65ed02a73f3923
tushgee <tushgee@gmail.com>
2020-01-14 11:11:58 -0800

    Added views, implemented services. closes #56

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-[![CircleCI](https://circleci.com/gh/tushgee/sfg-pet-clinic.svg?style=svg)](https://circleci.com/gh/tushgee/sfg-pet-clinic)
-
+[![CircleCI](https://circleci.com/gh/tushgee/sfg-pet-clinic/tree/master.svg?style=svg)](https://circleci.com/gh/tushgee/sfg-pet-clinic/tree/master)
 # sfg-pet-clinic
 
 SFG-NB Pet Clinic
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repository/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repository/OwnerRepository.java
@@ -4,8 +4,12 @@ import guru.springframework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastname);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
