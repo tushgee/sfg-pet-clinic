bc67107319e4efb1cee8ab85a56685e916a59e57
tushgee <tushgee@gmail.com>
2020-01-14 11:32:22 -0800

    adding model attributes to owner controller. #56

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/PetTypeService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/PetTypeService.java
@@ -3,4 +3,5 @@ package guru.springframework.sfgpetclinic.service;
 import guru.springframework.sfgpetclinic.model.PetType;
 
 public interface PetTypeService extends CrudService<PetType, Long> {
+
 }
